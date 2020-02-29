875d50923d1e9fe8b4591d3db88cf20a03402d85
MarSavar <Marbbk123x*>
2020-02-29 12:08:02 +0000

    Added tests

---
+++ fraction/FractionImplTest.java
@@ -0,0 +1,56 @@
+package fraction;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FractionImplTest {
+
+    @Test
+    void add() {
+    }
+
+    @Test
+    void subtract() {
+    }
+
+    @Test
+    void multiply() {
+    }
+
+    @Test
+    void divide() {
+    }
+
+    @Test
+    void abs() {
+    }
+
+    @Test
+    void negate() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void testClone() {
+    }
+
+    @Test
+    void inverse() {
+    }
+
+    @Test
+    void compareTo() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
